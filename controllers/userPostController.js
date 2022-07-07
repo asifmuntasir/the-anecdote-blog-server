d3fdd0b5debb30c5b87612940958dafb3ad4c310
asif-96 <shuyeb.dk@gmail.com>
2022-07-07 20:51:39 +0600

    store user post in mongodb

--- controllers/userPostController.js
+++ controllers/userPostController.js
@@ -1,14 +1,17 @@
+const { v4: uuidv4 } = require('uuid');
 const formidable = require('formidable');
+const fs = require('fs');
+const Post = require('../models/Post');
 
 module.exports.createPost = (req, res) => {
     const form = formidable({
         multiples: true
     });
-    form.parse(req, (error, fields, files) => {
+    form.parse(req, async (error, fields, files) => {
         // return res.json(fields);
         // return res.json(files);
         const errors = [];
-        const { title, body, description, id, user, slug } = fields;
+        const { title, body, description, id, name, slug } = fields;
         if (title === '') {
             errors.push({
                 msg: 'Title is required'
@@ -29,9 +32,61 @@ module.exports.createPost = (req, res) => {
                 msg: 'Slug is required'
             })
         }
+        if (Object.keys(files).length === 0) {
+            errors.push({
+                msg: 'Image is required',
+            })
+        } else {
+            const { mimetype } = files.image;
+            // console.log(mimetype);
+            const split_Image = mimetype.split('/');
+            // console.log(split_Image[1]);
+            const extension = split_Image[1].toLowerCase();
+            if (extension !== 'png' && extension !== 'jpg' && extension !== 'jpeg') {
+                errors.push({
+                    msg: `${extension} is not a valid extension`,
+                })
+            } else {
+                files.image.originalFilename = uuidv4() + '-' + extension;
+            }
+        }
+
+        const checkSlug = await Post.findOne({ slug });
+        if (checkSlug) {
+            errors.push({
+                msg: 'Please choose a unique slug/URL'
+            })
+        }
+
         if (errors.length !== 0) {
             return res.status(400).json({
-                errors
+                errors,
+                files
+            })
+        } else {
+            const newPath = __dirname + `../../the-anecdote-client/public/image/${files.image.originalFilename}`;
+            fs.copyFile(files.image.filepath, newPath, async (error) => {
+                try {
+                    const response = await Post.create({
+                        title,
+                        body,
+                        image: files.image.originalFilename,
+                        description,
+                        slug,
+                        userName: name,
+                        userId: id
+                    });
+                    return res.status(200).json({
+                        msg: 'Post created successfully',
+                        response
+                    })
+                    console.log('Image Successfully Uploaded.');
+                } catch (error) {
+                    return res.status(500).json({
+                        errors: error,
+                        msg: error.message
+                    })
+                }
             })
         }
     })
