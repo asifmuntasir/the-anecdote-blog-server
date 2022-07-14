b2e65c78cbab32b3553cc30fe11af7fcc9c0b77e
asif-96 <shuyeb.dk@gmail.com>
2022-07-15 00:04:39 +0600

    verify user token

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../utils/auth')
 const { createPost } = require("../controllers/userPostController")
-router.post('/create_post', createPost);
+router.post('/create_post', auth, createPost);
 
 module.exports = router;
