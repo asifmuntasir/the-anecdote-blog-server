f08c788d9c73aed62661f75ebe11ab8f439ef27a
asif-96 <shuyeb.dk@gmail.com>
2022-07-28 18:37:47 +0600

    backend complete for update user name

--- controllers/profileRoutes.js
+++
@@ -1,3 +0,0 @@
-module.exports.updateName = (req, res) => {
-
-}
--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const auth = require('../utils/auth');
 
-const { updateName } = require('../controllers/profileRoutes');
+const { updateName } = require('../controllers/profileController');
 
 
 router.post('/update_name', auth, updateName);
